bd6a636d6fc55094135b86e23ff88dc083ace937
mykyy <mykyy@mykyy-desktop>
2015-11-08 15:50:02 +0100

    test seo

--- src/Tom/AdminBundle/Controller/SettingsController.php
+++ src/Tom/AdminBundle/Controller/SettingsController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace Tom\AdminBundle\Controller;
-
+use Tom\SiteBundle\Entity\Seo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Tom\AdminBundle\Form\Type\SeoType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/ustawienia")
@@ -36,10 +38,39 @@ class SettingsController extends Controller
     * 
     * @Template("TomAdminBundle:Settings/Seo:Seo.html.twig")
     */
-    public function SeoAction()
+    public function SeoAction(Request $request)
     {
-        return array(
-            'pageTitle' => 'SEO <small>ustawienia witryny</small>'
-        );
+        $id=1;
+        $request = $this->get('request');
+
+    if (is_null($id)) {
+        $postData = $request->get('Seo');
+        $id = $postData['id'];
+    }
+
+    $em = $this->getDoctrine()->getManager();
+    $seo = $em->getRepository('TomSiteBundle:Seo')->find($id);
+    $form = $this->createForm(new SeoType(), $seo);
+
+    if ($request->getMethod() == 'POST') {
+        $form->bindRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as save the object to the database
+                        $em->persist($seo);
+                        $em->flush();
+                 //$this->addFlash('mykyy_form_notice','Artykuł został przesłany poprawnie!');
+            return $this->redirect($this->generateUrl('tom_admin_settings'));
+        }
+    }
+    
+
+    return $this->render('TomAdminBundle:Settings/Seo:Seo.html.twig', array(
+        'pageTitle' => 'SEO <small>ustawienia witryny</small>',
+        'form' => $form->createView()
+    ));
+//        return array(
+//            'pageTitle' => 'SEO <small>ustawienia witryny</small>'
+//        );
     }
 }
---
+++ src/Tom/AdminBundle/Form/Type/SeoType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tom\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SeoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+                    {
+        $builder->add('descriptions', 'text');
+        $builder->add('keywords', 'text');
+        //$builder->add('keywords','text');
+        //$builder->add('created', 'date');
+        //$builder->add('body','textarea');
+        
+        
+    }
+    public function getName() {
+        return 'create';
+    }
+
+}
