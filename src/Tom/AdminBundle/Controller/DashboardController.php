99fece57210bf74684588d6443988cc0d72ffd9c
mykyy <mykyy@mykyy-desktop>
2015-11-18 20:02:59 +0100

    początki messenger

--- src/Tom/AdminBundle/Controller/DashboardController.php
+++ src/Tom/AdminBundle/Controller/DashboardController.php
@@ -7,25 +7,67 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Tom\AdminBundle\Form\Type\MessengerType;
+
 class DashboardController extends Controller
 {
    /**
     * @Route(
-    *       "/",
+    *       
     *       name="tom_admin_dashboard"
+    *       
     * )
     * 
     * @Template()
     */
     public function indexAction()
     {
-        $user = $this->getUser();
+    $RepoScript = $this->getDoctrine()->getRepository('TomSiteBundle:Messenger');
+   // $Script     = $RepoScript->find($id);
+        $request = $this->getRequest();
+    $messengerform = $this->createForm( new MessengerType() );
+ 
+    if ( $request->isMethod( 'POST' ) ) {
+        $messengerform->submit( $request ); 
+        if ( $messengerform->isValid( ) ) { 
+           /*
+            * $data['contents']
+            */
+            $em = $this->getDoctrine()->getManager();
+                //$em->persist($Script);
+                $em->flush();
+            $data = $messengerform->getData(); 
+            $response['success'] = true;
+        }else{
+            $response['success'] = false;
+            $response['cause'] = 'whatever';
+        }
+            return new JsonResponse( $response );
+        }
+        return array(
+        'messengerform' => $messengerform->createView(),
+        'pageTitle' => 'Dashboard <small>najnowsze zdarzenia</small>',
+        );
+}
+        
+        
+        
+    
+ /*       $user = $this->getUser();
 
          // the above is a shortcut for this
         $user = $this->get('security.token_storage')->getToken()->getUser();
+        $postmessenger = $this->createForm( new MessengerType() );
+ 
+    return array(
+      'postmessenger' => $postmessenger->createView(),
+        'pageTitle' => 'Dashboard <small>najnowsze zdarzenia</small>',
+    );
         return array(
             'pageTitle' => 'Dashboard <small>najnowsze zdarzenia</small>',
             'user' => $user
         );
-    }
+    }*/
 }
--- src/Tom/AdminBundle/Form/Type/MessengerType.php
+++ src/Tom/AdminBundle/Form/Type/MessengerType.php
@@ -1,26 +1,19 @@
-<?php
-namespace AppBundle\Form\Type;
-
+<?php //
+namespace Tom\AdminBundle\Form\Type;
+ 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-
+ 
 class MessengerType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('contents', 'textarea')
-        ;
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-
-        });
-    }
-
-    public function getName()
-    {
-        return 'messenger';
-    }
+  public function buildForm( FormBuilderInterface $builder,
+                                            array $options )
+  {
+    $builder->add( 'contents', 'text' );
+    //$builder->add( 'body',  'textarea' );
+  }
+ 
+  function getName() {
+    return 'MessengerType';
+  }
 }
